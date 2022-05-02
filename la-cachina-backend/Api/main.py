77cfbf2587ced85209bbd5dc4c542cde370a8e90
1ElmoMazo1 <mamanijosue72@gmail.com>
2022-05-02 11:36:57 -0500

    Create main.py
    api en ypthon... posiblemente lo cambie todo para hoy en la noche

---
+++ la-cachina-backend/Api/main.py
@@ -0,0 +1,15 @@
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/la-cachina-backend/Api/<id>')
+def users_action(id):
+    print(request.method)
+    print("me solisitaron: " + id)
+    return id
+
+
+app.run(debug=True)
