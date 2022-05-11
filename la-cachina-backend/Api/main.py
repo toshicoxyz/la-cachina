5941ca822f8d20ec2ee0db7e8753b6f713f2e40c
Toshico <colcapedro@gmail.com>
2022-05-11 09:58:53 -0500

    XD

--- la-cachina-backend/Api/main.py
+++ la-cachina-backend/Api/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/la-cachina-backend/Api/<id>')
 def users_action(id):
     print(request.method)
-    print("me solicitaron: " + id)
+    print("Me solicitaron: " + id)
     return id
 
 
